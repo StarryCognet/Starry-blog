a97fbd52c62d5c314d27fe65d8c582a619be5bdb
StarryCognet <127642222+StarryCognet@users.noreply.github.com>
2025-10-27 07:08:28 +0000

    Deploying to gh-pages from @ StarryCognet/Starry-blog@1665210e5d42469030c0e3898810e70c37996ad0 🚀

---
+++ assets/chat.html-BkpYEdAB.js
@@ -0,0 +1,23 @@
+import{_ as e,c as n,d as a,a as o,e as i,r,o as p}from"./app-8OvGNvK4.js";const c={},s={class:"chat-fullscreen"};function m(l,d){const t=r("ChatBox");return p(),n("div",null,[a(` <style>
+.vp-content:not(.custom) {
+  max-width: none !important;
+  margin: 0 !important;
+  padding: 0 !important;
+}
+
+/* 确保聊天室占满整个视口 */
+main {
+  padding: 110px 0 0 0 !important;
+  margin: 0 !important;
+}
+
+/* 针对聊天室页面的特殊样式 */
+.chat-fullscreen {
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 200px;
+  left: 0;
+  z-index: 1000;
+}
+</style> `),o("div",s,[i(t)])])}const f=e(c,[["render",m]]),g=JSON.parse('{"path":"/chat.html","title":"聊天室","lang":"zh-CN","frontmatter":{"title":"聊天室","toc":false,"sidebar":false,"pageInfo":false,"breadcrumb":false,"contributors":false,"editLink":false,"lastUpdated":false,"prev":false,"next":false,"comment":false,"head":[["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"聊天室\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2025-10-26T15:50:48.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"StarryCognet\\",\\"url\\":\\"https://starrycognition.cn\\"}]}"],["meta",{"property":"og:url","content":"https://mister-hope.github.io/chat.html"}],["meta",{"property":"og:site_name","content":"StarryCognition"}],["meta",{"property":"og:title","content":"聊天室"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2025-10-26T15:50:48.000Z"}],["meta",{"property":"article:modified_time","content":"2025-10-26T15:50:48.000Z"}]]},"git":{"createdTime":1760683300000,"updatedTime":1761493848000},"readingTime":{"minutes":0.33,"words":99},"filePathRelative":"chat.md","excerpt":"<!-- <style>\\n.vp-content:not(.custom) {\\n  max-width: none !important;\\n  margin: 0 !important;\\n  padding: 0 !important;\\n}\\n\\n/* 确保聊天室占满整个视口 */\\nmain {\\n  padding: 110px 0 0 0 !important;\\n  margin: 0 !important;\\n}\\n\\n/* 针对聊天室页面的特殊样式 */\\n.chat-fullscreen {\\n  width: 100vw;\\n  height: 100vh;\\n  position: fixed;\\n  top: 200px;\\n  left: 0;\\n  z-index: 1000;\\n}\\n</style> -->"}');export{f as comp,g as data};
